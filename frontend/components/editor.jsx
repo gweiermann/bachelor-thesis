8619f29ab95d57798bbb818478a45dd74ec166b6
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2025-03-19 10:40:19 +0100

    make editor with readonly lines so that the function prototype can't be modified

---
+++ frontend/components/editor.jsx
@@ -0,0 +1,47 @@
+"use client"
+
+import { constrainedEditor } from "constrained-editor-plugin";
+import MonacoEditor from '@monaco-editor/react';
+import { useRef } from 'react'
+
+export default function Editor({ functionProtoype, placeholder }) {
+    const monacoRef = useRef(null);
+
+    function handleEditorDidMount(editor, monaco) {
+        monacoRef.current = editor;
+
+        const constrainedInstance = constrainedEditor(monaco);
+        const model = editor.getModel();
+        let restrictions = []
+        constrainedInstance.initializeIn(editor);
+        restrictions.push({
+            range: [1, 1, 2, 1],
+            allowMultiline: true
+        });
+
+        constrainedInstance.addRestrictionsTo(model, [
+            {
+                range: [3, 1, 4, 1],
+                allowMultiline: true,
+            }
+        ])
+    }
+
+    return (
+        <MonacoEditor
+            height="90vh"
+            language="cpp"
+            onMount={handleEditorDidMount}
+            defaultValue={`${functionProtoype}\n{\n  ${placeholder}\n}`}
+            options={{
+                minimap: {
+                    enabled: false
+                }
+            }}
+        />
+    )
+}
+// Within React Component
+
+
+
