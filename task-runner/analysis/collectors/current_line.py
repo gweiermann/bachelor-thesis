9014f03641540d01a1835d6bdab486510081c1e4
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2025-04-29 18:17:08 +0200

    fixed highlighted line is off by 3, 2 line swap detection is not working at the moment

--- task-runner/analysis/collectors/current_line.py
+++ task-runner/analysis/collectors/current_line.py
@@ -2,13 +2,12 @@ from .collector import Collector
 
 class CurrentLine(Collector):
     def setup(self, frame):
-        self.line_offset = frame.GetLineEntry().GetLine() - 3
         self.previous_line = 0
         self.current_line = 0
 
     def pre_step(self, frame):
         self.current_line = self.previous_line
-        self.previous_line = frame.GetLineEntry().GetLine() - self.line_offset
+        self.previous_line = frame.GetLineEntry().GetLine()
     
     def step(self, frame):
         return self.current_line
